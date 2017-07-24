b984e54ba98c473e7ffe749c65ed527c2b83d557
James Bergin <javascriptjames23@gmail.com>
2017-07-24 15:09:18 +0100

    All working no AI needs private methods setup

--- computer.rb
+++ computer.rb
@@ -7,8 +7,11 @@ class Computer
 		@right_colour_wrong_place = 0
 		@wrong_place_and_colour = 0
 	end
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+#ssecret code is generated.
 
 	def generate_numbers
+		@code =[]
 
 	  4.times do
 		gen = rand(7)
@@ -16,7 +19,9 @@ class Computer
 	  end
 	return @code
 	end
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
+private
 	def print_choices
 		@options.each do
 		  
@@ -28,7 +33,8 @@ class Computer
 		  end
 		end
 	end
-
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+#checks if the user choice is in the the choices array, ie. is it a valide entry.
 	def guess_in_array(x)
 		  if @options.include?(x)
 		  	return true
@@ -36,19 +42,24 @@ class Computer
 		      puts "This was not a valid color please choice again"
 		  end
 	end
-
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+#this is the tricky part, this makes a duplicate of the guess and the generated code.
+#it then checks if any of the geues is a direct match if so it changes the guess entry
+#to checked. This then passes the sample array again through to look through as right colour wrong position match.
 	def check_user_guess(arr)
 		samplecode = @code.dup
+		sampleguess = arr.dup
 		
-		arr.each_with_index do
+		sampleguess.each_with_index do
 			|colour,i|
 			if samplecode.include?(colour) == true && i == samplecode.find_index(colour)
 				x =samplecode.find_index(colour)
 				samplecode[x] = "checked"
+				sampleguess[i] = "codematched"
 				@right_right_colour_position += 1
 			end
 		end
-		print samplecode
+	
 
 		arr.each_with_index do
 			|colour, i|
@@ -56,12 +67,12 @@ class Computer
 				@right_colour_wrong_place += 1
 				
 				 g = samplecode.find_index(colour)
-				 samplecode[g] ="halfcheck"
+				 samplecode[g] = "halfcheck"
 			end
 		end
 		return samplecode
 	end
-
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 	def print_user_guess(arr)
 		arr.each do
@@ -71,28 +82,22 @@ class Computer
 			  print "#{colour} | "
 		end
 	end
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 	def reset_counter
 		@right_right_colour_position = 0
 		@right_colour_wrong_place = 0
 		@wrong_place_and_colour = 0
 	end
-
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
  	def return_results(arr)
- 		puts "your guess is:"
- 		print_user_guess(arr)
+ 		puts "\nYOUR GUESS IS:" 
+ 		print_user_guess(arr)		
 		puts "\nThe result of the guess are:"
 		puts "#{@right_right_colour_position} of your guess are the the RIGHT color in the RIGHT position"
 		puts "#{@right_colour_wrong_place} of your guess are the the RIGHT color in the WRONG position"
+		@wrong_place_and_colour = 4 -(@right_right_colour_position + @right_colour_wrong_place)
 		puts "#{@wrong_place_and_colour} of your guess just WRONG."
 		
 	end
-
-
-
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 end
-=begin
-a=Computer.new
-a.generate_numbers
-a.check_user_guess(["blue","green","blue","red"])
-a.return_results(["blue","green","blue","red"])
-=end
--- game.rb
+++ game.rb
@@ -1,27 +1,29 @@
 class Game
-		attr_accessor :guess_code
+		attr_accessor :guess_code, :name
 	load 'player.rb'
 	load 'computer.rb'
 
-	def initialize
-		
+	def initialize	
 		@player 
 		@computer = Computer.new
 		@guess_code =[]
 		@win =["checked","checked","checked","checked"]
 	end
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 	def welcome
 		puts "Welcome to Mastermind the game of sheer brain power!"
 		puts "The aim of the game is to guess the secret code in 12 guesses."
 		puts "are you up for the challege?\n"
 	end
-
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 	def get_name
 		puts "\nHi Player what is your name: "
-		gets.chomp
+		@name = gets.chomp
 	end
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
 	def enter_choice
      @computer.print_choices
      count = 1		
@@ -29,44 +31,49 @@ class Game
 		    puts "Enter a colour for slot number #{count}"
 		    guess_input = gets.chomp.downcase
 		    if @computer.guess_in_array(guess_input) == true
-			  @guess_code.push(guess_input)
-		      p @guess_code
-		      count += 1
-		      p "count is #{count}"
-
+			  @guess_code.push(guess_input)		    
+		      count += 1		      
 		    end
-
 		end
-
 	end
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
 	def check_win(results)
 		win =["checked","checked","checked","checked"]
 		if results == win
 		puts "Winner Winner Chicken Dinner"
 			return true
 		end
-	end
+	end 
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
 	def play_again
 		puts "Press Y to play again, or any other key to quit."
 		answer = gets.chomp.capitalize
 			if answer == "Y"
+			reset_guess
+			@computer.generate_numbers()
+			@computer.reset_counter
 			start_game
 				
 			else
 				abort ("Thanks for Playing!")
 			end
 	end
-
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 	def reset_guess
 		@guess_code = []
 	end
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+
 	def start_game
 		count = 1
 		welcome()
 		@player = Player.new(get_name)
 		@computer.generate_numbers()
 		while count < 13
+		  puts "\n#{name.upcase} THIS IS GUESS NUMBER #{count}"
 		  enter_choice
 		  results = @computer.check_user_guess(@guess_code)
 		  @computer.return_results(@guess_code)
@@ -75,15 +82,17 @@ class Game
 		  end
 		  reset_guess
 		  @computer.reset_counter
+
 		count += 1
 		end
-		puts "Sorry you loose"
-		puts " the code was #{@computer.code}"
+		puts "Sorry you loose #{name}"
+		puts "The code was #{@computer.code}"
 		play_again
 
 	end
 
 end
+#<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
 
 a=Game.new
 a.start_game
